63bb10632277f524f4a56600781732b80d903d34
mbasrid <bsrdmrc0011@gmail.com>
2022-07-12 20:36:38 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+/*
+
+var ad = prompt("Adınız: ");
+var ogrno = prompt("Öğrenci Numaranız: ");
+var dersadi = prompt("Ders adı giriniz: ");
+var not1 = prompt("Vize Notunuz: ");
+var not2 = prompt("Final Notunuz: ");
+var ort = ((Number(not1) * 0.3) + (Number(not2)*0.7))/2;
+
+console.log(ogrno + " numaralı " + ad + " adlı öğrencinin " + dersadi + " not ortalaması " + ort);/*
+
+*/
+
+
+var x = 10;
+var y = 20;
+var z = 10;
+
+console.log(x*(y+z));
+
+x--;
+console.log(x);
+
+var sonuc = x>y;
+console.log(sonuc);
+
